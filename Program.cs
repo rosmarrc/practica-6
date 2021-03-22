51090d7d73791d1413f987412f85f274197a94c2
rosmarrc <79125757+rosmarrc@users.noreply.github.com>
2021-03-22 17:43:30 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace pract_6_eje_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ClaseC obj = new ClaseC();
+            Console.WriteLine(obj);
+
+            Console.ReadKey();
+        }
+    }
+
+    public class ClaseA
+    {
+        public ClaseA()
+            { Console.WriteLine("soy el constructor de la clase A");
+            }
+    }
+    class ClaseB:ClaseA
+    {
+        public ClaseB()
+        {
+            Console.WriteLine("soy el constructor de la clase B");
+        }
+    }
+
+    class ClaseC:ClaseB
+    {
+        public ClaseC()
+        {
+            Console.WriteLine("soy el constructor de la clase C");
+        }
+    }
+}
